386e06475c1ab636ac27481e8e8516137ab23f2b
Angel Cai <angelcai1818@gmail.com>
2022-04-19 19:12:59 -0600

    working tic tac toe game

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ client.on('messageCreate', (message) => {
     }
 })
 
-/* Tic Tac Toe */
+/* Start Tic Tac Toe Game */
 let EMPTY = Symbol("empty");
 let PLAYER = Symbol("player");
 let BOT = Symbol("bot");
 let tictactoe_state
 
+//Creates initial board for tictactoe
 function makeGrid() {
     components = []
     for (let row = 0; row < 3; row++){
@@ -52,22 +53,125 @@ function makeGrid() {
     }
     return components
 }
+
+//Gets random int for bot to choose
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
+//Game Logic for a draw or game is over
+function isDraw() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++){
+            if (tictactoe_state[row][col] == EMPTY){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+function isGameOver(){
+    for (let row = 0; row < 3; row++) {
+        if ((tictactoe_state[row][0] == tictactoe_state[row][1]) 
+            && (tictactoe_state[row][1] == tictactoe_state[row][2]) 
+            && (tictactoe_state[row][2] != EMPTY)) {
+                return true;
+            }
+        
+        if ((tictactoe_state[0][row] == tictactoe_state[1][row]) 
+            && (tictactoe_state[1][row] == tictactoe_state[2][row])
+            && (tictactoe_state[2][row] != EMPTY)) {
+                return true;
+            }
+    }
+    if (tictactoe_state[1][1] != EMPTY) {
+        if ((tictactoe_state[0][0] == tictactoe_state[1][1] && tictactoe_state[1][1] == tictactoe_state[0][2])
+            || (tictactoe_state[2][0] == tictactoe_state[1][1] && tictactoe_state[1][1] == tictactoe_state[0][2])){
+            return true;
+        }
+    }
+    return false;
+}
+
+//Player's move/player clicked on button
+client.on('interactionCreate', async interaction => {
+    if(!interaction.isButton()) return;
+    if(!interaction.customId.startsWith('tictactoe')) return;
+
+    // Player choice
+    let parsedFields = interaction.customId.split('_')
+    let row = parsedFields[1]
+    let col = parsedFields[2]
+
+    if (tictactoe_state[row][col] != EMPTY) {
+        interaction.update({
+            content: "This spot has already been taken",
+            components: makeGrid()
+        })
+    }
+
+    tictactoe_state[row][col] = PLAYER
+    if(isGameOver()){
+        interaction.update({
+            content: "Congrats, You Won!!!!",
+            components: makeGrid()
+        })
+        return;
+    }
+    if(isDraw()){
+        interaction.update({
+            content: "draw!",
+            components: makeGrid()
+        })
+        return;
+    }
+    
+
+    // Bot choice
+    let botRow
+    let botCol
+    do {
+        botRow = getRandomInt(2)
+        botCol = getRandomInt(2)
+    } while(tictactoe_state[botRow][botCol] != EMPTY);
+
+    tictactoe_state[botRow][botCol] = BOT;
+    if(isGameOver()){
+        interaction.update({
+            content: "Congrats, You Won!!!!",
+            components: makeGrid()
+        })
+        return;
+    }
+    if(isDraw()){
+        interaction.update({
+            content: "draw!",
+            components: []
+        })
+        return;
+    }
+    
+
+    interaction.update({
+        components: makeGrid()
+    })
+})
+
+//User has used /tictactoe command
 client.on('interactionCreate', async interaction => {
     if(!interaction.isCommand()) return;
 
     const { commandName } = interaction;
 
     if (commandName === 'tictactoe') {
-        
-
         tictactoe_state = [
-            [EMPTY, EMPTY, BOT],
             [EMPTY, EMPTY, EMPTY],
-            [PLAYER, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
         ]
 
         await interaction.reply({ content: 'Playing a game of tic-tac-toe!', components: makeGrid() });
     }
-
 })
+/* End Tic Tac Toe Game */
+
 client.login(token)
